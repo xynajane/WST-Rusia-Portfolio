de17a2aafe056d2798d424f1049652f54ddba0fd
xynajane <122327989+xynajane@users.noreply.github.com>
2024-04-25 12:34:32 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,23 +1,46 @@
 $(document).ready(function(){
-$('.slider').slick({
-    arrows:false,
-    dots:true,
-    appendDots:'.slider-dots',
-    dotsClass:'dots'
+  $('.slider').slick({
+      arrows: false,
+      dots: true,
+      appendDots: '.slider-dots',
+      dotsClass: 'dots'
+  });
+
+  let hamburger = document.querySelector('.hamberger'); 
+  let times = document.querySelector('.times');
+  let mobileNav = document.querySelector('.mobile-nav');
+
+  // Function to open mobile menu
+  function openMobileNav() {
+      mobileNav.classList.add('open');  
+  }
+
+  // Function to close mobile menu
+  function closeMobileNav() {
+      mobileNav.classList.remove('open');  
+  }
+
+  // Toggle mobile menu when hamburger icon is clicked
+  hamburger.addEventListener('click', function(){
+      if (mobileNav.classList.contains('open')) {
+          closeMobileNav();
+      } else {
+          openMobileNav();
+      }
+  });
+
+  // Close mobile menu when close icon is clicked
+  times.addEventListener('click', function(){
+      closeMobileNav();
+  });
+
+  // Close mobile menu when clicking anywhere on the screen outside the menu
+  document.addEventListener('click', function(event) {
+      if (!mobileNav.contains(event.target) && !hamburger.contains(event.target)) {
+          closeMobileNav();
+      }
+  });
 });
 
 
-let hamberger = document.querySelector('.hamberger');
-let times = document.querySelector('.times');
-let mobileNav = document.querySelector('.mobile-nav');
-
-hamberger.addEventListener('click', function(){
-  mobileNav.classList.add('open');  
-});
-
-times.addEventListener('click', function(){
-    mobileNav.classList.remove('open');  
-});
-
-});
 
